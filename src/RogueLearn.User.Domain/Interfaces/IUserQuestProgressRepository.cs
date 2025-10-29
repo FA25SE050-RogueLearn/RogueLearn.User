dc86d96c5a8700ac34ce5ec335cf19b13f299833
Thinh An <thinhan1701@gmail.com>
2025-10-29 23:15:10 +0700

    fix LINQ operations

--- src/RogueLearn.User.Domain/Interfaces/IUserQuestProgressRepository.cs
+++ src/RogueLearn.User.Domain/Interfaces/IUserQuestProgressRepository.cs
@@ -5,5 +5,6 @@ namespace RogueLearn.User.Domain.Interfaces;
 
 public interface IUserQuestProgressRepository : IGenericRepository<UserQuestProgress>
 {
-
+    // NEW METHOD: A specialized method to efficiently fetch progress for a user and a specific list of quests.
+    Task<IEnumerable<UserQuestProgress>> GetUserProgressForQuestsAsync(Guid authUserId, List<Guid> questIds, CancellationToken cancellationToken = default);
 }
